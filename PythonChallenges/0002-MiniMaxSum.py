dfe62a0aa059272dea63a24c5a9f6f89d6293e0c
Matthew Mason <m.mason29272@gmail.com>
2025-04-01 15:27:05 +0100

    Updated README and setup minimaxsum challenge

---
+++ PythonChallenges/0002-MiniMaxSum.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    print("worked")
+
+if __name__ == '__main__':
+
+    # arr = list(map(int, input().rstrip().split()))
+    arr = list(map(int, "1 2 3 4 5".rstrip().split()))
+
+    miniMaxSum(arr)
